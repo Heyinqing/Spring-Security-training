1e89dde145c048a642502bf8c32c2d024dd3c827
hyq <heyinqing171526@163.com>
2023-07-26 17:59:15 +0800

    maven引入

---
+++ Security-training/src/main/java/com/rw/SecurityTrainingApplication.java
@@ -0,0 +1,11 @@
+package com.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTrainingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityTrainingApplication.class,args);
+    }
+}
